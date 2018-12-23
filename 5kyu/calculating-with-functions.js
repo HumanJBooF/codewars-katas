82242b93808c3794df1575ac0deda809a0954ee8
humanjboof <humanjboof@gmail.com>
2018-12-23 00:14:58 -0500

    updated calc with func

--- 5kyu/calculating-with-functions.js
+++ 5kyu/calculating-with-functions.js
@@ -16,11 +16,49 @@
 // The most outer function represents the left operand, the most inner function represents the right operand
 // Divison should be integer division, e.g eight(dividedBy(three())) / eight(divided_by(three)) should return 2, not 2.666666...
 
+// ~~~~ first solution ~~~~
+// const expr = (number, operation) => {
+//     if (!operation) 
+//         return number;
+//     return operation(number);
+// }
+
+// const zero = operation => expr(0, operation);
+// const one = operation => expr(1, operation);
+// const two = operation => expr(2, operation);
+// const three = operation => expr(3, operation);
+// const four = operation => expr(4, operation);
+// const five = operation => expr(5, operation);
+// const six = operation => expr(6, operation);
+// const seven = operation => expr(7, operation);
+// const eight = operation => expr(8, operation);
+// const nine = operation => expr(9, operation);
+
+// const plus = a => b => b + a
+// const minus = a => {
+//     return b => {
+//         return b - a;
+//     }
+// }
+// const times = a => {
+//     return b => {
+//         return a * b;
+//     }
+// }
+// const dividedBy = a => {
+//     return b => {
+//         return Math.floor(b / a);
+//     }
+// }
+
+// ~~~~ Refactor ~~~~
 const expr = (number, operation) => {
-    if (!operation)
+    if (!operation) {
         return number;
+    }
     return operation(number);
 }
+
 
 const zero = operation => expr(0, operation);
 const one = operation => expr(1, operation);
@@ -33,23 +71,7 @@ const seven = operation => expr(7, operation);
 const eight = operation => expr(8, operation);
 const nine = operation => expr(9, operation);
 
-const plus = a => {
-    return b => {
-        return b + a;
-    }
-}
-const minus = a => {
-    return b => {
-        return b - a;
-    }
-}
-const times = a => {
-    return b => {
-        return a * b;
-    }
-}
-const dividedBy = a => {
-    return b => {
-        return Math.floor(b / a);
-    }
-}
+const plus = a => b => b + a
+const minus = a => b => b - a;
+const times = a => b => a * b;
+const dividedBy = a => b => Math.floor(b / a);
